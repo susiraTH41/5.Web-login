ad77910d0500b8b1970df15015cac56ec34d8533
susiraTH41 <78338819+susiraTH41@users.noreply.github.com>
2021-11-26 17:34:58 +0700

    11/26/2021 5:34 PM

--- project/view.py
+++ project/view.py
@@ -1,6 +1,6 @@
 from project import app
-from flask import Flask , request , render_template
-
+from flask import Flask , request , render_template , flash
+from passlib.hash import sha256_crypt
 import psycopg2
 
 conn = psycopg2.connect(
@@ -10,24 +10,53 @@ conn = psycopg2.connect(
     user=app.config["DB_USER"],
     password = app.config["DB_PASSWORD"])
 
-user = ["susira","tulapongsa"]
+user=[]
 
 @app.route('/')
 def index():
+    global user
     if user == [] :
         return login() 
-    
-    cur = conn.cursor()
-    s = "SELECT email,first_name,last_name,user_id FROM info_user"
-    cur.execute(s)
-    list_users = cur.fetchall()
-    return render_template('index.html', list_users = list_users , user = user)
+    try :
+        cur = conn.cursor()
+        s = "SELECT email,first_name,last_name,user_id FROM info_user"
+        cur.execute(s)
+        list_users = cur.fetchall()
+        print(list_users)
+        cur.close
+        return render_template('index.html', list_users = list_users , user = user)
+    except e :
+        return render_template('index.html', list_users = list_users , user = user)
 
 
 @app.route('/login')
 def login():
-    user = []
     return render_template('login.html',title='Login') 
+
+
+@app.route('/userLogin', methods=["POST"])
+def userLogin():
+    if request.method == 'POST':
+        email=request.form["email"]
+        password=request.form["password"]
+        if not email :
+            return render_template('login.html',title='Login') 
+        if not password :
+            return render_template('login.html',title='Login') 
+        try :
+            with conn: 
+                cur = conn.cursor()
+                query = "SELECT * FROM info_user WHERE email = '" + email + "' ;"
+                cur.execute(query)
+                global user
+                user = cur.fetchall()
+                cur.close
+                if sha256_crypt.verify(password,user[0][4]):
+                    return index()
+                else :
+                    return login() 
+        except e :
+            return login() 
 
 
 @app.route('/register')
@@ -37,22 +66,33 @@ def register():
 
 @app.route('/getRegis', methods=["POST"])
 def getRegis():
-    cursor = conn.cursor()
-    fname=request.form.get("fname")
-    lname=request.form.get("lname")
-    email=request.form.get("email")
-    password=request.form.get("password")
-    passwordcon=request.form.get("passwordcon")
-    if password != passwordcon :
-        return render_template('register.html',title='Register111')  
-    query = """INSERT INTO info_user(user_id, first_name, last_name, email ,pass)
-            VALUES(DEFAULT, %s ,%s ,%s , %s)"""
-    values = ( fname, lname, email, password)
-    cursor.execute(query, values)
-    cursor.close()
+    if request.method == 'POST':
+        
+        fname=request.form["fname"]
+        lname=request.form["lname"]
+        email=request.form["email"]
+        password=request.form["password"]
+        passwordcon=request.form["passwordcon"]
+        if not fname :
+            return render_template('register.html',title='Register111')  
+        if not lname :
+            return render_template('register.html',title='Register111')  
+        if not email :
+            return render_template('register.html',title='Register111')  
+        if password != passwordcon  :
+            flash("The password don't match")
+            return render_template('register.html',title='Register111') 
+        password = sha256_crypt.encrypt(password) 
+        try :
+            with conn: 
+                cur = conn.cursor()
+                query = """INSERT INTO info_user(user_id, first_name, last_name, email ,pass)
+                        VALUES(DEFAULT, %s ,%s ,%s , %s)"""
+                values = ( fname, lname, email, password)
+                cur.execute(query, values)
+                conn.commit()
+                cur.close()
 
-
- 
-    return register() 
-
-
+                return register() 
+        except e :
+            return register() 
